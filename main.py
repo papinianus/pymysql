661df765aab856173f2c7f6c8a0454e6926c6ddf
papinianus <dglmsksooah@gmail.com>
2020-10-19 10:51:23 +0900

    set up

---
+++ main.py
@@ -0,0 +1,26 @@
+import json
+import os
+from os.path import join, dirname
+import dotenv
+import logging
+import pymysql.cursors
+
+dotenv_path = join(dirname(__file__), '.env')
+dotenv.load_dotenv(dotenv_path)
+
+# 環境変数の値をAPに代入
+DB_Host = os.environ.get("host")
+DB_User = os.environ.get("user")
+DB_Password = os.environ.get("pw")
+DB_Name = os.environ.get("dbname")
+
+
+
+def lambda_handler(event, context):
+    connection = pymysql.connect(host=DB_Host,
+                                 user=DB_User,
+                                 password=DB_Password,
+                                 db=DB_Name,
+                                 charset='utf8',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    connection.close()
